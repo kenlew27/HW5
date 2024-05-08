ffd00fcf28fe80e03332e64ae8fcb1c552178439
kenlew27 <155922857+kenlew27@users.noreply.github.com>
2024-05-07 21:56:51 -0500

    Add files via upload

--- pa5.py
+++ pa5.py
@@ -1,41 +1,37 @@
-import math
-
-
 def gcd(a,b):
+    '''
+    GCD function with two number input and modulus operation
+    '''
     if a==b:
         return a
-    elif a < b:
+    if a < b:
         return gcd(b,a)
-    elif b == 0:
+    if b == 0:
         return a
-    else:
-        return gcd(b, a%b)
+    return gcd(b, a%b)
 
 def bisection_root(function,a,b):
+    '''
+    Bisection Function with function and two bounds input
+    '''
     median = (a+b)/2
     vala = function(a)
     valb = function(b)
     valm = function(median)
-    if (vala > 0 and valb > 0) or (vala < 0 and valb < 0):
+    if (vala and valb > 0) or (vala and valb < 0):
         raise ValueError
-    if (valm>0 and valm<0.0000001) or (valm<0 and valm> -0.0000001):
+    if (valm>0 and valm<0.0000001) or (valm<0 and valm > -0.0000001):
         return median
-    elif (valm < 0 and vala < 0) or (valm >  0 and vala > 0):
+    if (valm and vala < 0 ) or (valm and vala >  0 ):
         return bisection_root(function,median,b)
-    else:
-        return bisection_root(function,a,median)
-        
-def remove_pairs(str):
-    if len(str)==0:
+    return bisection_root(function,a,median)
+
+def remove_pairs(strg):
+    '''
+    Remove pairs of elements within string
+    '''
+    if len(strg)==0:
         return ""
-    if str[0:2] == "EW" or str[0:2] == "WE" or str[0:2] == "NS" or str[0:2] == "SN":
-        return remove_pairs(str[2:])
-    else:
-        return str[0] + remove_pairs(str[1:])
-
-
-
-
-
-
-
+    if strg[0:2] == "EW" or strg[0:2] == "WE" or strg[0:2] == "NS" or strg[0:2] == "SN":
+        return remove_pairs(strg[2:])
+    return strg[0] + remove_pairs(strg[1:])
